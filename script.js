0628a3d17545763378fa509ebd7bd4e6dc227816
kshipra-jadav <jadavkshipra2@gmail.com>
2022-10-22 14:19:36 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,85 @@
+let botBtn = document.getElementById("bot-button")
+let chatApp = document.getElementById("chatapp")
+let botBtnImage = document.querySelector("#bot-button img")
+let botsend = document.querySelector("#chatapp-form")
+let querytxt = document.querySelector("#chatapp-input-text")
+let chatContainer = document.getElementById("chatapp-chat-container")
+
+setTimeout(() => loadingDone(), 1000)
+
+const URL = "https://gsfc-chatbot-backend.onrender.com/get-bot-ans"
+
+function loadingDone() {
+	botBtn.disabled = false
+}
+
+botBtn.addEventListener("click", () => {
+	firstClick()
+	chatApp.classList.toggle("chatapp-closed")
+})
+
+$("#chatapp-input-send-btn").click(processQuery)
+$("#chatapp-input-text").on("keypress", (e) => {
+	if (e.which == 13) {
+		processQuery()
+	}
+})
+
+async function processQuery() {
+	const query = $("#chatapp-input-text").val()
+
+	const response = await fetch(URL, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({ query: query }),
+	})
+
+	const body = await response.json()
+	successfunc(body)
+}
+
+async function firstClick() {
+	firstClick = noop
+	const query = "gsfc video"
+
+	const response = await fetch(URL, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({ query: query }),
+	})
+
+	const body = await response.json()
+	successfunc(body)
+	firstResponse()
+	document
+		.getElementsByClassName("chatapp-chat-message chatapp-chat-message--sent")
+		.item(0)
+		.remove()
+}
+function noop() {} //!! No Operation Function --> Required to only call the firstClick function once.
+
+function successfunc(result) {
+	let chatDiv = document.createElement("div")
+	chatDiv.innerText = result.data.query
+	chatDiv.classList.add("chatapp-chat-message", "chatapp-chat-message--sent")
+	chatContainer.appendChild(chatDiv)
+	querytxt.value = ""
+
+	let chatDivBot = document.createElement("div")
+	chatDivBot.innerHTML = result.data.answer
+	chatDivBot.classList.add("chatapp-chat-message")
+	chatContainer.appendChild(chatDivBot)
+
+	chatContainer.scrollTop = chatContainer.scrollHeight
+}
+
+function firstResponse() {
+	let chatDivBot = document.createElement("div")
+	chatDivBot.innerText = "Hello There, How Can I Help You?"
+	chatDivBot.classList.add("chatapp-chat-message")
+	chatContainer.appendChild(chatDivBot)
+}
